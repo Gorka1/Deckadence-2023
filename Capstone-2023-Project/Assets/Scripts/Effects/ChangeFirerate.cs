1a21edf73f2085ae725f6c49c0858309c215d0dc
Jorge-A-Velasquez <jav494@nyu.edu>
2022-10-11 11:32:03 -0400

    Added two basic card effects

--- Capstone-2023-Project/Assets/Scripts/Effects/ChangeFirerate.cs
+++ Capstone-2023-Project/Assets/Scripts/Effects/ChangeFirerate.cs
@@ -2,15 +2,14 @@ using System.Collections;
 using System.Collections.Generic;
 using UnityEngine;
 
-public class IncreaseDamage : AbstractEffect
+public class ChangeFirerate : AbstractEffect
 {
     [SerializeField]
-    int value = 1;
+    float value = 1f;
     public override void MainEffect(GameObject GO) {
-        Debug.Log("Applied effect to object: " + GO.name);
         GunManager GM = GO.GetComponent<GunManager>();
         if (GM != null) {
-            GM.gunStats.dmg += value;
+            GM.gunStats.fireRate += value;
         }
     }
 }
