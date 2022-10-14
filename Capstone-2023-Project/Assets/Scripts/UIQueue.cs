1f4478876fc4f818a50316c4f089fa431f97ef58
Jorge-A-Velasquez <jav494@nyu.edu>
2022-10-14 15:33:30 -0400

    Added card moving script to signal a completed quest

--- Capstone-2023-Project/Assets/Scripts/UIQueue.cs
+++ Capstone-2023-Project/Assets/Scripts/UIQueue.cs
@@ -8,6 +8,10 @@ public class UIQueue : MonoBehaviour
     [SerializeField]
     CardObj[] cards;
     public Image[] cardsSprites;
+    [SerializeField]
+    List<Vector3> SpriteStartPos;
+    [SerializeField]
+    float CardYShift = 20f;
 
 
     public QueueManager queueManager;
@@ -15,7 +19,10 @@ public class UIQueue : MonoBehaviour
     // Start is called before the first frame update
     void Start()
     {
-
+        SpriteStartPos = new List<Vector3>();
+        for (int i = 0; i < cardsSprites.Length; i++) {
+            SpriteStartPos.Add(cardsSprites[i].gameObject.GetComponent<RectTransform>().position);
+        }
     }
 
     // Update is called once per frame
@@ -28,6 +35,15 @@ public class UIQueue : MonoBehaviour
             if (cards[i] != null)
             {
                 cardsSprites[i].sprite = cards[i].GetData().cardGraphic;
+                if (cards[i].GetQuestStatus()) {
+                    cardsSprites[i].gameObject.GetComponent<RectTransform>().SetPositionAndRotation(
+                        new Vector3(SpriteStartPos[i].x, SpriteStartPos[i].y + CardYShift, SpriteStartPos[i].z),
+                        Quaternion.identity);
+                } else {
+                    cardsSprites[i].gameObject.GetComponent<RectTransform>().SetPositionAndRotation(
+                        SpriteStartPos[i],
+                        Quaternion.identity);
+                }
                 print(cards[i].GetData().cardGraphic);
             }
         }
