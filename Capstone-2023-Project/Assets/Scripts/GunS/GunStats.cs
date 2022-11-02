4ffb930dc020a0cdc3303a4b76fb9de8a3aab87b
Jorge-A-Velasquez <jav494@nyu.edu>
2022-11-02 12:27:58 -0400

    Added GunEnums

--- Capstone-2023-Project/Assets/Scripts/GunS/GunStats.cs
+++ Capstone-2023-Project/Assets/Scripts/GunS/GunStats.cs
@@ -7,9 +7,10 @@ public class GunStats
 {
     public int dmg = 0;
     public float fireRate = 0f;
-    public string fireType = "basic";
     public float range = 0f;
-    public bool automatic = false;
+    public GunEnums.FireType fireType = GunEnums.FireType.Single;
     public int ammo = 0;
     public float speed = 0f;
+    public float critChance = 0f;
+    public float critBonus = 0f;
 }
