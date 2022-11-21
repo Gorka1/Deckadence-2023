1216935fb3d34677f73ae05e78fe2fec37fff0e4
Jorge-A-Velasquez <jav494@nyu.edu>
2022-11-21 13:44:49 -0500

    Added arena system

---
+++ Capstone-2023-Project/Assets/Scripts/Managers/ArenaManager.cs
@@ -0,0 +1,47 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class ArenaManager : MonoBehaviour
+{
+    [SerializeField]
+    List<GameObject> Walls;
+    [SerializeField]
+    List<GameObject> Enemies;
+    GameManager GM;
+    bool isActive = false;
+
+    private void Start() {
+        GM = GameObject.FindGameObjectWithTag("GameManager").GetComponent<GameManager>();
+    }
+
+    private void Update() {
+        if (isActive) {
+            int numOfNull = 0;
+            foreach (GameObject e in Enemies) {
+                if (e == null) {
+                    numOfNull += 1;
+                }
+            }
+            if (Enemies.Count == numOfNull) {
+                isActive = false;
+                foreach (GameObject w in Walls) {
+                    w.SetActive(false);
+                }
+            }
+        }
+    }
+
+    private void OnTriggerEnter(Collider other) {
+        if (other.transform.CompareTag("Player")) {
+            isActive = true;
+            GM.GetPlayer().GetComponent<PlayerManager>().inCombat = true;
+            foreach (GameObject w in Walls) {
+                w.SetActive(true);
+            }
+            foreach (GameObject e in Enemies) {
+                e.SetActive(true);
+            }
+        }
+    }
+}
