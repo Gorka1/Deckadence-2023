41d46b214834f179d5dea5a431dbeac7ea908f21
TokyoOni <83197853+TokyoOni@users.noreply.github.com>
2022-12-05 12:51:36 -0500

    Win State and Screen in Test Level
    Done by having an empty trigger collision box the player must overlap

---
+++ Capstone-2023-Project/Assets/Scripts/Level/WinLevel.cs
@@ -0,0 +1,32 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.SceneManagement;
+
+public class WinLevel : MonoBehaviour
+{
+    // Start is called before the first frame update
+    void Start()
+    {
+
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        if (Input.GetKeyDown(KeyCode.R)) // loads scene on user input From anywhere
+        {
+            SceneManager.LoadScene(2);
+        }
+    }
+
+    private void OnTriggerEnter(Collider collision)
+    {
+        if (collision.tag == "Player")
+        {
+            SceneManager.LoadScene(3);
+        }
+    }
+
+    
+}
