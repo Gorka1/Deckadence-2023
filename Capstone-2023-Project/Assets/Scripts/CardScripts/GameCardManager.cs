56f774c0661e77c2dfe7f5a8eb0c15e5490c2229
Jorge-A-Velasquez <jav494@nyu.edu>
2022-12-10 13:49:42 -0500

    Added GameCardManager object

---
+++ Capstone-2023-Project/Assets/Scripts/CardScripts/GameCardManager.cs
@@ -0,0 +1,23 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+[System.Serializable]
+public class GameCardData {
+    [SerializeField]
+    public CardData CD;
+    [SerializeField]
+    public int count;
+}
+public class GameCardManager : MonoBehaviour
+{
+    [SerializeField]
+    public List<GameCardData> MainCardList;
+    public List<CardData> CurrCardList;
+    public int maxDeckSize = 10;
+
+    void Start()
+    {
+        DontDestroyOnLoad(this.gameObject);
+    }
+}
