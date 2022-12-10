1c41119be15415adcf0546a349830ef974ea1550
jaden <cep450@nyu.edu>
2022-12-10 16:22:08 -0500

    juiced losing health cards ui

--- Capstone-2023-Project/Assets/Scripts/UI/UIHealth.cs
+++ Capstone-2023-Project/Assets/Scripts/UI/UIHealth.cs
@@ -1,4 +1,5 @@
 using System.Collections.Generic;
+using System.Collections;
 using UnityEngine;
 using UnityEngine.UI;
 
@@ -18,8 +19,33 @@ public class UIHealth : MonoBehaviour {
             Debug.Log("Tried to remove hp card but none left");
             return;
         }
-        Destroy(hpCards[hpCards.Count - 1]);
+        StartCoroutine(DropCard(hpCards[hpCards.Count - 1].transform));
         hpCards.RemoveAt(hpCards.Count - 1);
+    }
+
+    //Animate card off and kill it
+    //-x^2 + 2x
+    IEnumerator DropCard(Transform card) {
+
+        float distScale = 30f;
+        float timeScale = 15f;
+        float timeLimit = 50f;
+
+        float t = 0;
+        Vector3 posOrig = card.position;
+
+        while(t < timeLimit) {
+
+            t += Time.deltaTime * timeScale;
+
+            float yDiff = ( -(Mathf.Pow(t, 2)) + 2 * t ) * distScale;
+
+            card.position = new Vector3(posOrig.x, posOrig.y + yDiff, posOrig.z);
+
+            yield return null;
+        }
+
+        Destroy(card.gameObject);
     }
 
 }
