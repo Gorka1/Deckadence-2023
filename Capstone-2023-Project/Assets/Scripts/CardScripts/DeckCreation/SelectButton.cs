eb537cac6c78490d929cf6b0d5011a3f48b5b2b6
Jorge-A-Velasquez <jav494@nyu.edu>
2022-12-11 22:12:54 -0500

    Ported over DeckCreator

---
+++ Capstone-2023-Project/Assets/Scripts/CardScripts/DeckCreation/SelectButton.cs
@@ -0,0 +1,43 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.UI;
+
+public class SelectButton : MonoBehaviour
+{
+    // Start is called before the first frame update
+    public CardData currCard;
+    [SerializeField]
+    Image cardSprite;
+    [SerializeField]
+    TMPro.TextMeshProUGUI cardText;
+    bool setup = false;
+    public int count;
+    [SerializeField]
+    DeckCreator DC;
+
+    private void Start() {
+        DC = GameObject.FindGameObjectWithTag("DeckMenu").GetComponent<DeckCreator>();
+    }
+
+    private void Update() {
+        if (setup) {
+            cardText.text = currCard.cardName + " : " + count;
+        }
+    }
+
+    public void SetUp(CardData newCD, int newCount) {
+        currCard = newCD;
+        count = newCount;
+        cardText.text = currCard.cardName + " : " + count;
+        cardSprite.sprite = newCD.cardGraphic;
+        setup = true;
+    }
+
+    public void OnClick() {
+        bool ret_val = DC.SelectCard(currCard);
+        if (ret_val) { count -= 1; }
+    }
+
+    public void DestroySelf() { Destroy(this.gameObject); }
+}
